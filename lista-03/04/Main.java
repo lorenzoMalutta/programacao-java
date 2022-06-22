785ed136c8831bd8591c4d2aee431b892728bda3
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2022-06-22 13:11:31 -0300

    Add files via upload

---
+++ lista-03/04/Main.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    Cartao c1 = new Cartao(10);
+    System.out.println(c1.comprarCafe(1));
+    System.out.println(c1.getUnidade());
+  }
+}
