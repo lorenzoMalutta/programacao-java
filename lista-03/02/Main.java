260d48b04f0c55f6b911698ab9921e650e7c0ed5
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2022-06-24 10:39:48 -0300

    Update Main.java

--- lista-03/02/Main.java
+++ lista-03/02/Main.java
@@ -1,5 +1,6 @@
 class Main {
   public static void main(String[] args) {
+    //Casa.getProprietario().getNome();
     Proprietario p1 = new Proprietario(188545, "Ronaldo");
     Casa c1 = new Casa(4324, "Sao paulo", 156.445, p1);
     System.out.println(p1.getNome());
